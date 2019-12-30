b999aab5dfded4ed9d9c85dedd8b0c2bb54ea95d
Ronak Dhoot <ronak.dhoot@mailsphere.co.uk>
2019-12-31 01:45:21 +0530

    With DB migrations, models and seeders

--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 # org-stat
 Github Org Stats!
 
-##Pre
+##Pre-requisite
+- PHP 7.x
+- https://getcomposer.org/download/
 
 
 ##Setup
+Run composer to install dependencies.
+```
+composer install
+```
 
+
+Add your github API keys to the config file. You can set up a personal access token [here](https://github.com/settings/tokens).
+
+```
+cp .env.example .env
+vim .env
+```
+- update **GITHUB_PERSONAL_ACCESS_TOKEN_USER** 
+- update **GITHUB_PERSONAL_ACCESS_TOKEN_PASSWORD** 
+- update **GITHUB_ORGANISATION**
+ 
+##Running the import script
+
+Import your repository commit data into the local SQLite database by running the import script from your command line:
+```
+php -f src/import.php
+```
+
+##Viewing the web interface
+You can access the web interface through PHP's built in web-server. To use PHP's webserver run the following command from your command line:
+```
+php artisan serve
+```
+
+You should then be able to access the web interface through [http://localhost:8000](http://localhost:8000)
 
 ##Reference
 - https://developer.github.com/v3/
+- https://github.com/KnpLabs/php-github-api
+- https://laravel.com/docs/6.x
 
 
 ## Code of Conduct
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Github\Client;
+use Github\HttpClient;
+use App\Repository;
+use App\Author;
+use App\WeeklyCommit;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +17,94 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $client = new Client(
+            new HttpClient\Builder(array('cache_dir' => '/tmp/github-api-cache'))
+        );
+
+        $client->authenticate(env('GITHUB_PERSONAL_ACCESS_TOKEN_USER'), env('GITHUB_PERSONAL_ACCESS_TOKEN_PASSWORD'), Client::AUTH_HTTP_PASSWORD);
+
+        echo "......................................................".PHP_EOL;
+        echo "..................Fetching repositories...............".PHP_EOL;
+        echo "......................................................".PHP_EOL;
+
+        // Relevant documentation:
+        // https://developer.github.com/v3/repos/#list-organization-repositories
+        // https://github.com/KnpLabs/php-github-api/blob/master/lib/Github/Api/Repo.php
+        // https://github.com/KnpLabs/php-github-api/blob/master/doc/result_pager.md
+
+        $organizationApi = $client->api('organization');
+        $paginator  = new \Github\ResultPager($client);
+        $parameters = array(env('ORGANISATION'));
+        $repos      = $paginator->fetchAll($organizationApi, 'repositories', $parameters);
+
+// Insert repos
+
+        foreach ($repos AS $repo) {
+            echo $repo['name'] ."\n";
+
+            $commits = $client->api('repo')->commits()->all(env('ORGANISATION'), $repo['name'], array('sha' => 'master'));
+
+            Repository::create(array(
+                'id' => $repo['id'],
+                'name' => $repo['name'],
+                'fullname' => $commits[0]['commit']['author']['name'],
+                'html_url' => $repo['html_url']
+            ));
+
+        }
+
+        echo "......................................................".PHP_EOL;
+        echo "...............Fetching weekly commits................".PHP_EOL;
+        echo "......................................................".PHP_EOL;
+
+        foreach($repos AS $repo) {
+
+            $repo_authors = $client->api('repo')->statistics(env('ORGANISATION'), $repo['name']);
+
+
+            $organizationApi = $client->api('repositories');
+            $paginator       = new \Github\ResultPager($client);
+            $parameters      = array(env('ORGANISATION'), $repo['name']);
+            $repo_authors    = $paginator->fetchAll($organizationApi, 'statistics', $parameters);
+
+
+            // Test query - SELECT datetime(week,'unixepoch'), * FROM weekly_commits AS wc INNER JOIN authors AS a ON wc.author_id = a.id INNER JOIN repos AS r ON wc.repo_id=r.id WHERE login='cmorillo' ORDER BY week ASC;
+
+            echo "******* ".$repo['name'] ."[".count($repo_authors)." authors] *******\n";
+
+            foreach ($repo_authors AS $author_data) {
+
+                echo $author_data['author']['login'] . " [" . $author_data['total'] . "]\n";
+
+                // Insert author
+
+                Author::create(array(
+                    'id' => $author_data['author']['id'],
+                    'username' => $author_data['author']['login'],
+                    'avatar_url' => $author_data['author']['avatar_url'],
+                    'html_url' => $author_data['author']['html_url']
+                ));
+
+
+                foreach ($author_data['weeks'] AS $weekly_commits)  {
+
+                    // Insert weekly commit information
+
+                    if ($weekly_commits['c'] > 0) {
+
+                        WeeklyCommit::create(array(
+                            'repo_id' => $repo['id'],
+                            'author_id' => $author_data['author']['id'],
+                            'week' => $weekly_commits['w'],
+                            'additions' => $weekly_commits['a'],
+                            'deletions' => $weekly_commits['d'],
+                            'commits' => $weekly_commits['c']
+                        ));
+
+                    }
+                }
+            }
+        }
     }
 }
